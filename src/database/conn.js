7b4908e7cb1a64366229493c3dd1aac0a7d362d2
Atishay Jain <hritikjain707@gmail.com>
2021-05-02 20:43:44 +0530

    Fix: Working server

---
+++ src/database/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const uri = process.env.DB_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Error in connecting Database", err));
