ca0b96af2e37915008b83ce843134323d490e534
Atishay Jain <hritikjain707@gmail.com>
2021-05-08 12:52:59 +0530

    Repasted code, Fixing PR Issue

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-// FOR THE PR ISSUE, WILL CLEAN LATER
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fillProductsCollection = require("./utils/databaseInit");
+const productRouter = require("./routers/productRouter");
+
+require("dotenv").config();
+require("./database/conn");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", productRouter);
+
+//** To be used once for flooding product data **
+// fillProductsCollection();
+
+app.get("/", (req, res) => {
+  res.send("dicePizza API server is running");
+});
+
+app.listen(process.env.PORT || 8000, () =>
+  console.log("Server Started on Port 8000")
+);
